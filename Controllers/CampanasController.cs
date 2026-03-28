6fe0005bd8aa498369c31c28bcd4fb14925f2230
Faus-DTA <fausto.mm192025@gmail.com>
2026-03-28 12:17:32 -0500

    unificar filtros y detalle para resolver conflicto

--- Controllers/CampanasController.cs
+++ Controllers/CampanasController.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Pc_01.Data;
 using Pc_01.Models;
-using System.Linq;
 
 namespace Pc_01.Controllers
 {
     public class CampanasController : Controller
     {
+        // Método Index con Filtros (Funcionalidad 1)
         public IActionResult Index(string categoria, string estado)
         {
             var campanias = CampaniaData.Campanias.AsQueryable();
@@ -22,6 +22,19 @@ namespace Pc_01.Controllers
             }
 
             return View(campanias.ToList());
+        }
+
+        // Método Details (Funcionalidad 2 - ¡Agregado!)
+        public IActionResult Details(int id)
+        {
+            var campania = CampaniaData.Campanias.FirstOrDefault(c => c.Id == id);
+            
+            if (campania == null)
+            {
+                return NotFound();
+            }
+            
+            return View(campania);
         }
     }
 }
